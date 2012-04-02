eb11834cb6c5856ff140d4d948797b947032f0b2
Diogo N Silva <o.diogosilva@gmail.com>
2012-04-02 22:13:14 +0100

    teste

--- 4Pipe4.py
+++ 4Pipe4.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with 4Pipe4.  If not, see <http://www.gnu.org/licenses/>.
 
+COCO!
+
 import subprocess
 import os
 import sys
