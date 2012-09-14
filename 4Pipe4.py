5a4a33850824f58a42da4616cc588b930fa8d61f
Diogo <o.diogosilva@gmail.com>
2012-09-14 18:23:16 +0100

    Removed old files

--- 4Pipe4.py
+++ 4Pipe4.py
@@ -14,9 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with 4Pipe4.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys,re,subprocess
-
-sys.path.append("/home/diogo/Python/Modules")
 
 import subprocess
 import os
