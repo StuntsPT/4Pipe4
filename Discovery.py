ee35ad465de903cddb46c3fe933f9571b38bf7ee
Francisco Pina Martins <f.pinamartins@gmail.com>
2012-09-19 11:52:04 +0100

    Finished implementing the variation finding routine and started implementing the TCS output.

--- Discovery.py
+++ Discovery.py
@@ -16,13 +16,13 @@
 
 def cafParse(infile_name):
 	#Parses the .caf file and returns the information we are looking for.
-	textfile = open(infile_name,'r')
+	caffile = open(infile_name,'r')
 	datatype = 0
 	skip = 0
 	contigreads = {}
 	contigs = {}
 
-	for lines in textfile:
+	for lines in caffile:
 		#Define lines to skip
 		if skip > 0:
 			skip -= 1
@@ -42,7 +42,6 @@ def cafParse(infile_name):
 			datatype = 3
 			contigname = lines[lines.rindex(" "):].strip()
 			contigs[contigname] = []
-			#contigreads = {}
 			skip = 2
 			continue
 
@@ -90,12 +89,19 @@ def cafParse(infile_name):
 			if lines.startswith("\n"):
 				contigs[contigname].append(contigqual)
 				skip = 1
+
+	caffile.close()
+	
 	#We return the following dictionary:
 	#{contig_name:[{read_name:[sequence, [qualities], position]}, contig_seq, [contig quals]]}
 	#The sequences are already returned in R&C position if necessary.
 	return contigs
 
 def FindSNPs(contigs):
+	#Finds tha variation present in the dict that resulted from the parsing of
+	#the .caf file.
+	#It looks nasty, but wroks well and relatively fast.
+	var_info = {}
 	for k,v in contigs.items():
 		contig_name = k
 		contig_map = v[0]
@@ -104,25 +110,29 @@ def FindSNPs(contigs):
 		padded_var = set()
 		unpadded_var = set()
 		contig_variants = {}
-		#print(contig_name)
-		for names,read_info in contig_map.items():
+		
+		for read_info in contig_map.values():
 			padded, unpadded = StringCompare(contig_seq.upper(), read_info[0], read_info[2])
 			for i,j in zip(padded, unpadded):
 				padded_var.add(i)
 				unpadded_var.add(j)
-			if contig_name.endswith("_c35"):
-				print(names)
-				print(padded_var)
-				print(contig_seq[read_info[2]-1:])
-				print(read_info[0])
-		#print(padded_var)
-			#for position in padded_var:
-				#if reads[2] <= position and reads[2] + len(reads[0]) >= position:
-					#contig_variants[position] = {"A":[],"C":[],"G":[],"T":[],"-":[]}
-					#if position in variants:
-						#contig_variants[position] = 
-					#else:
-						#variants[position] = reads[0][position]
+
+		contig_variants = contig_variants.fromkeys(padded_var, {"A":[],"C":[],"G":[],"T":[],"-":[]})
+
+		for read_info in contig_map.values():
+			#Yes, we are looping through the same as before, but I don't see an
+			#alternative
+			for base_pos in range(read_info[2],len(read_info[0])):
+				if base_pos in contig_variants:
+					try:
+						contig_variants[base_pos][read_info[0][base_pos]].append(read_info[1][base_pos])
+					except:
+						print("WARNING: Your READS have ambiguities - in this case an \"%s\" in the contig %s in position %s.\n" % (read_info[0][base_pos], contig_name, base_pos))
+
+		sorted_unpadded = list(unpadded_var).sort()
+		var_info[contig_name] = [contig_variants, sorted_unpadded]
+		
+	return var_info
 	
 def StringCompare(contig, read, position):
 	#Compares the contig and read sequence and returns the variant positions
@@ -134,11 +144,27 @@ def StringCompare(contig, read, position):
 	return pad_var, unpad_var
 
 def RevComp(sequence):
-	#Reverses and complements a DNA sequence
+	#Reverses and complements a DNA sequence. IUPAC codes are NOT considered,
+	#but they should not be present in 454 data anyway
 	comp_table = str.maketrans("ACGT","TGCA")
 	rc_seq = str.translate(sequence,comp_table)[::-1]
 	return rc_seq
 
+def TCSwriter(infile_name, variation):
+	#Writes the bases with variation to a new TCS file
+	infile_name = infile_name[:infile_name.rindex(".")] + ".short1.tcs"
+	outfile = open(infile_name,'w')
+	outfile.write("#TCS V1.0")
+	outfile.write("#")
+	outfile.write("# contig name			padPos	upadPos| B  Q |	tcov covA covC covG covT cov* | qA qC qG qT q* |  S |	Tags")
+	outfile.write("#")
+	for k,v in variation.items():
+		print(k)
+
+	outfile.close()
+
 infile_name = "/home/francisco/Desktop/4PipeTest/TestData_assembly/TestData_d_results/TestData_out.caf" #Hardcoded for now
 contigs = cafParse(infile_name)
-FindSNPs(contigs)
+variation = FindSNPs(contigs)
+TCSwriter(infile_name, variation)
+
