cfceca8ac9303d5a59f5d8ef5e138a46bd1f7445
Francisco Pina Martins <f.pinamartins@gmail.com>
2012-12-11 10:30:13 +0000

    Continued implementing the helper script.

--- helper-scripts/user-installer.sh
+++ helper-scripts/user-installer.sh
@@ -19,13 +19,15 @@
 #required by 4Pipe4 in user space, except Blast2GO.
 #Please consult the Readme.md file for more information on the programs used by
 #4Pipe4.
+
 #Define some variables:
 #URLs:
 sff_extract_url="http://bioinf.comav.upv.es/_downloads/sff_extract_0_3_0"
 seqclean_url="http://sourceforge.net/projects/seqclean/files/seqclean-x86_64.tgz/download"
 mira_url="http://sourceforge.net/projects/mira-assembler/files/MIRA/stable/mira_3.4.1.1_prod_linux-gnu_x86_64_static.tar.bz2/download"
+#Please select the correct one for your distribution
 emboss_url="" #Deb
-emboss_url="" #RPM
+#emboss_url="" #RPM
 blast_url="ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.2.27+-x64-linux.tar.gz"
 p7zip_url="http://sourceforge.net/projects/p7zip/files/p7zip/9.20.1/p7zip_9.20.1_x86_linux_bin.tar.bz2/download"
 
@@ -48,14 +50,27 @@ echo "Extracting and locally installing the downloaded programs... Please wait."
 #Gzipped:
 for i in $(ls ~/Software |grep .gz)
 do
-tar xvfz ~/Software/$i -C ~/Software
+tar xfz ~/Software/$i -C ~/Software
 done
 #Bzipped
 for i in $(ls ~/Software |grep .bz2)
 do
-tar xvfj ~/Software/$i -C ~/Software
+tar xfj ~/Software/$i -C ~/Software
+done
+#Deb
+for i in $(ls ~/Software |grep .deb)
+do
+ar vx ~/Software/$i data.tar.gz | tar xz -C ~/Software
+done
+#RPM
+for i in $(ls ~/Software |grep .rpm)
+do
+~/Software/p7zip/7z x ~/Software/$i -o ~/Software/
 done
 
+echo "If no errors accurred, (dead links, etc..) all of the software required\
+run 4Pipe4 is now installed in userspace. Please add the correct paths to \
+your 4Pipe4rc file."
 
 getorf (http://emboss.sourceforge.net/apps/cvs/emboss/apps/getorf.html)
 etandem (http://helixweb.nih.gov/emboss/html/etandem.html)
