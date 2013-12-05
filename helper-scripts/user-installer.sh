10a6655f5ac8ee13407f01d3f4791edfd6f0e8bd
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-12-05 14:08:10 +0000

    Updated to mira4

--- helper-scripts/user-installer.sh
+++ helper-scripts/user-installer.sh
@@ -25,7 +25,7 @@
 #URLs:
 sff_extract_url="http://bioinf.comav.upv.es/_downloads/sff_extract_0_3_0"
 seqclean_url="http://sourceforge.net/projects/seqclean/files/seqclean-x86_64.tgz/download"
-mira_url="http://sourceforge.net/projects/mira-assembler/files/MIRA/stable/mira_3.4.1.1_prod_linux-gnu_x86_64_static.tar.bz2/download"
+mira_url="http://sourceforge.net/projects/mira-assembler/files/MIRA/stable/mira_4.0rc4_linux-gnu_x86_64_static.tar.bz2/download"
 blast_url="ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.28/ncbi-blast-2.2.28+-x64-linux.tar.gz"
 p7zip_url="http://sourceforge.net/projects/p7zip/files/p7zip/9.20.1/p7zip_9.20.1_x86_linux_bin.tar.bz2/download"
 
