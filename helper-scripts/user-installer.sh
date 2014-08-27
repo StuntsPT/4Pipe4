fc4206f023c522c1be8f0485735452d458bb7617
Francisco Pina Martins <f.pinamartins@gmail.com>
2014-08-27 16:35:55 +0100

    Updated installer scripts to include pysam and cython.

--- helper-scripts/user-installer.sh
+++ helper-scripts/user-installer.sh
@@ -28,6 +28,10 @@ seqclean_url="http://sourceforge.net/projects/seqclean/files/seqclean-x86_64.tgz
 mira_url="http://sourceforge.net/projects/mira-assembler/files/MIRA/stable/mira_4.0.2_linux-gnu_x86_64_static.tar.bz2/download"
 blast_url="ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.28/ncbi-blast-2.2.28+-x64-linux.tar.gz"
 p7zip_url="http://sourceforge.net/projects/p7zip/files/p7zip/9.20.1/p7zip_9.20.1_x86_linux_bin.tar.bz2/download"
+#Temporary for pysam:
+cython_url="https://github.com/cython/cython/archive/0.21b1.tar.gz"
+pysam_url="https://github.com/pysam-developers/pysam.git"
+
 
 #Create a dir for your new programs (change this to your preference):
 workdir=~/Software
@@ -36,6 +40,7 @@ dldir=$workdir/compressed
 mkdir -p $workdir/sff_extract
 mkdir -p $dldir
 
+
 #Download the programs form the web
 echo "Downloading programs from their respective websites... Please wait."
 wget -c $sff_extract_url -O $workdir/sff_extract/sff_extract
@@ -43,6 +48,8 @@ wget -c $seqclean_url -O $dldir/seqclean-x86_64.tgz
 wget -c $mira_url -O $dldir/mira_4.0rc4_linux-gnu_x86_64_static.tar.bz2
 wget -c $blast_url -P $dldir/
 wget -c $p7zip_url -O $dldir/p7zip_9.20.1_x86_linux_bin.tar.bz2
+wget -c $cython_url -P $dldir/
+
 
 #Extract and prepare the downloaded programs:
 #sff_extract
@@ -58,6 +65,20 @@ for i in $(ls $dldir |grep .bz2)
 do
 tar xfj $dldir/$i -C $workdir
 done
+
+
+#Temporary for pysam
+#Build cython
+cd $dldir/cython-0.21b1
+python3 setup.py install --user
+echo "cython was locally installed to ~/.local"
+
+#Download and build pysam
+git clone $pysam_url $workdir/pysam_url
+cd $workdir/pysam_url
+python3 setup.py install --user
+echo "pysam was locally installed to ~/.local"
+
 
 echo ""
 echo "If no errors occurred, (dead links, etc..) all of the software required \
